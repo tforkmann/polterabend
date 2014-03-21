1c2acb92c2e8691d42fce51bd1a4bdd2d3700631
Tim Forkmann <tforkmann@gmail.com>
2014-03-21 07:45:21 +0100

    added routes for api and hi, welcome page

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,19 @@ app.get('/', function(req, res){
 	res.send('Polterabend!');
 })
 
+app.get('/api', function(req, res){
+	res.send('Hello Express!');
+	res.send({name: 'Tim', age: 36})
+})
+
+app.get('/hi', function(req, res){
+	var message = [
+		"<h1> Hallo Polterabendgäste</h1>",
+		"<p> Willkommen auf der Polterabendseite von Sarah und Tim</p>",
+		"<ul><li>schnell</li>",
+		"<li>lustig</li>",
+		"<li>flexibel</li></ul>"].join("\n");
+	res.send(message);
+})
+
 require('./user');
