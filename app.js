c136d3f942d1f3e872dc621170f6d484370601fe
Tim Forkmann <tforkmann@gmail.com>
2014-03-21 10:14:05 +0100

    fixing less-middleware issue

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var lessMiddleware = require('less-middleware');
 var app = module.exports = express();
 //var seojs = require('express-seojs');
 
@@ -20,7 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-//app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(lessMiddleware(path.join(__dirname, 'source', 'less'), {
+  dest: path.join(__dirname, 'public', 'stylesheets'),
+  preprocess: {
+    path: function(pathname, req) {
+      return pathname.replace('/stylesheets', '');
+    }
+  }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(seojs('your_secret_api_token'));
 
